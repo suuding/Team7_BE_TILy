c9206dd6ce52a279d12846a4b3bedb3c3c6ab7b5
suuding <tngus4789@naver.com>
2023-09-26 03:13:56 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/example/tily/TiLyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiLyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiLyApplication.class, args);
+	}
+
+}
