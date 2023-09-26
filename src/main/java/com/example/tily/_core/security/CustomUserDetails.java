53af3293ecde387bb638f75f70afaa6cd6f3ab53
suuding <tngus4789@naver.com>
2023-09-26 21:52:04 +0900

    feat : globalExceptionhandler, globalValidationHandler 및 예외 클래스 설정

--- src/main/java/com/example/tily/_core/security/CustomUserDetails.java
+++ src/main/java/com/example/tily/_core/security/CustomUserDetails.java
@@ -8,9 +8,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Getter
