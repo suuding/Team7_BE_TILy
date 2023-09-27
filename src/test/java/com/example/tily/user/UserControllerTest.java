b324598c650d883a833c974b06a5c0723a368c39
suuding <tngus4789@naver.com>
2023-09-27 12:34:22 +0900

    test : 로그인, 비밀번호 재설정 테스트 코드 작성

--- src/test/java/com/example/tily/user/UserControllerTest.java
+++ src/test/java/com/example/tily/user/UserControllerTest.java
@@ -30,7 +30,7 @@ public class UserControllerTest {
 
         // given
         String email = "test1@nate.com";
-        UserRequest.EmailCheckDTO requestDTO = new UserRequest.EmailCheckDTO();
+        UserRequest.CheckEmailDTO requestDTO = new UserRequest.CheckEmailDTO();
         requestDTO.setEmail(email);
 
         String requestBody = om.writeValueAsString(requestDTO);
@@ -73,7 +73,7 @@ public class UserControllerTest {
         result.andExpect(jsonPath("$.message").value("ok"));
     }
 
-    @DisplayName("사용자_회원가입_실패_test_1 : 잘못된 이메일 형식")
+    @DisplayName("사용자_회원가입_실패_test_1:잘못된 이메일 형식")
     @Test
     public void user_join_fail_test_1() throws Exception {
 
@@ -97,7 +97,7 @@ public class UserControllerTest {
         result.andExpect(jsonPath("$.message").value("올바른 이메일 형식을 입력해주세요."));
     }
 
-    @DisplayName("사용자_회원가입_실패_test_2 : 잘못된 비밀번호 형식")
+    @DisplayName("사용자_회원가입_실패_test_2:잘못된 비밀번호 형식")
     @Test
     public void user_join_fail_test_2() throws Exception {
 
@@ -112,6 +112,142 @@ public class UserControllerTest {
         // when
         ResultActions result = mvc.perform(
                 post("/join")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(requestBody)
+        );
+
+        // then
+        result.andExpect(jsonPath("$.success").value("false"));
+        result.andExpect(jsonPath("$.message").value("올바른 비밀번호 형식을 입력해주세요."));
+    }
+
+    @DisplayName("사용자_로그인_성공_test")
+    @Test
+    public void user_login_success_test() throws Exception {
+
+        // given
+        UserRequest.LoginDTO requestDTO = new UserRequest.LoginDTO();
+        requestDTO.setEmail("tngus@pusan.ac.kr");
+        requestDTO.setPassword("meta1234!");
+
+        String requestBody = om.writeValueAsString(requestDTO);
+
+        // when
+        ResultActions result = mvc.perform(
+                post("/login")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(requestBody)
+        );
+
+        // then
+        result.andExpect(jsonPath("$.success").value("true"));
+    }
+
+    @DisplayName("사용자_로그인_실패_1:존재하지 않는 이메일")
+    @Test
+    public void user_login_fail_test_1() throws Exception {
+
+        // given
+        UserRequest.LoginDTO requestDTO = new UserRequest.LoginDTO();
+        requestDTO.setEmail("tngus@pusan.ac.kr");
+        requestDTO.setPassword("meta1234!");
+
+        String requestBody = om.writeValueAsString(requestDTO);
+
+        // when
+        ResultActions result = mvc.perform(
+                post("/login")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(requestBody)
+        );
+
+        // then
+        result.andExpect(jsonPath("$.success").value("false"));
+        result.andExpect(jsonPath("$.message").value("해당 이메일을 찾을 수 없습니다 : tngus1@nate.com"));
+    }
+
+    @DisplayName("사용자_로그인_실패_2:비밀번호 불일치")
+    @Test
+    public void user_login_fail_test_2() throws Exception {
+
+        // given
+        UserRequest.LoginDTO requestDTO = new UserRequest.LoginDTO();
+        requestDTO.setEmail("tngus@pusan.ac.kr");
+        requestDTO.setPassword("meta1234!!");
+
+        String requestBody = om.writeValueAsString(requestDTO);
+
+        // when
+        ResultActions result = mvc.perform(
+                post("/login")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(requestBody)
+        );
+
+        // then
+        result.andExpect(jsonPath("$.success").value("false"));
+        result.andExpect(jsonPath("$.message").value("비밀번호가 일치하지 않습니다. "));
+    }
+
+    @DisplayName("사용자_비밀번호_재설정_성공_test")
+    @Test
+    public void user_change_password_success_test() throws Exception {
+
+        // given
+        UserRequest.ChangePwdDTO requestDTO = new UserRequest.ChangePwdDTO();
+        requestDTO.setEmail("tngus@pusan.ac.kr");
+        requestDTO.setPassword("meta1234!!");
+
+        String requestBody = om.writeValueAsString(requestDTO);
+
+        // when
+        ResultActions result = mvc.perform(
+                post("/password/change")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(requestBody)
+        );
+
+        // then
+        result.andExpect(jsonPath("$.success").value("true"));
+    }
+
+    @DisplayName("사용자_비밀번호_재설정_실패_test_1:존재하지 않는 이메일")
+    @Test
+    public void user_change_password_fail_test_1() throws Exception {
+
+        // given
+        UserRequest.ChangePwdDTO requestDTO = new UserRequest.ChangePwdDTO();
+        requestDTO.setEmail("tngus1@pusan.ac.kr");
+        requestDTO.setPassword("meta1234!!");
+
+        String requestBody = om.writeValueAsString(requestDTO);
+
+        // when
+        ResultActions result = mvc.perform(
+                post("/password/change")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(requestBody)
+        );
+
+        // then
+        result.andExpect(jsonPath("$.success").value("false"));
+        result.andExpect(jsonPath("$.message").value("해당 이메일을 찾을 수 없습니다 : tngus1@pusan.ac.kr"));
+    }
+
+    @DisplayName("사용자_비밀번호_재설정_실패_test_2:잘못된 비밀번호 형식")
+    @Test
+    public void user_change_password_fail_test_2() throws Exception {
+
+        // given
+        UserRequest.ChangePwdDTO requestDTO = new UserRequest.ChangePwdDTO();
+        requestDTO.setEmail("tngus@pusan.ac.kr");
+        requestDTO.setPassword("meta1234");
+
+        String requestBody = om.writeValueAsString(requestDTO);
+
+        // when
+        ResultActions result = mvc.perform(
+                post("/password/change")
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .content(requestBody)
         );
