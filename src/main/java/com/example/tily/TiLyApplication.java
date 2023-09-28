eda33d634c8e0a1b4b63442d68f5980bee2eea6c
hoayii <lhh@hoyaiui-MacBookAir.local>
2023-09-28 13:06:46 +0900

    feat: 개인 로드맵 생성 구현테스트 코드 작성

--- src/main/java/com/example/tily/TiLyApplication.java
+++ src/main/java/com/example/tily/TiLyApplication.java
@@ -1,8 +1,17 @@
 package com.example.tily;
 
+import com.example.tily.user.Role;
+import com.example.tily.user.User;
+import com.example.tily.user.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Arrays;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -12,4 +21,20 @@ public class TiLyApplication {
 		SpringApplication.run(TiLyApplication.class, args);
 	}
 
+	@Profile("local")
+	@Bean
+	CommandLineRunner localServerStart(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			userRepository.saveAll(Arrays.asList(newUser("hong@naver.com", "hong", passwordEncoder)));
+		};
+	}
+
+	private User newUser(String email, String name, PasswordEncoder passwordEncoder){
+		return User.builder()
+				.email(email)
+				.name(name)
+				.password(passwordEncoder.encode("hongHong!"))
+				.role(Role.ROLE_USER)
+				.build();
+	}
 }
--- src/main/java/com/example/tily/roadmap/RoadmapRequest.java
+++ src/main/java/com/example/tily/roadmap/RoadmapRequest.java
@@ -2,13 +2,18 @@ package com.example.tily.roadmap;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class RoadmapRequest {
 
     @Getter @Setter
     public static class CreateIndividualDTO {
-        @NotNull
+        @NotBlank(message = "이름을 입력해주세요.")
+        @Size(min=2, max=20, message = "이름은 2자에서 20자 이내여야 합니다.")
         private String name;
     }
 }
