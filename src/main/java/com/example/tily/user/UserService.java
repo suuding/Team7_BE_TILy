3492f8dc2c99fd2d89f46c520569d8fe90aa9564
suuding <tngus4789@naver.com>
2023-10-01 18:13:20 +0900

    chore : 이메일 인증코드 전송 내용(형식) 메소드 분리

--- src/main/java/com/example/tily/user/UserService.java
+++ src/main/java/com/example/tily/user/UserService.java
@@ -118,20 +118,14 @@ public class UserService {
             // 랜덤한 인증코드 생성
             String code = createCode();
 
+            // 이메일로 인증코드 전송
             MimeMessage message = javaMailSender.createMimeMessage();
             message.addRecipients(Message.RecipientType.TO, email);
             message.setSubject("[TIL-y] 인증코드가 도착했습니다.");
-            String text = "<div style='background-color: #f8f9fa; padding: 10px 20px 50px 20px; margin: 20px'>" +
-                    "<h1 style='margin: 20px'>이메일 인증</h1>" +
-                    "<p>안녕하세요!<br>본인 인증을 위해 아래의 코드를 복사해 인증코드 입력 칸에 입력해주세요.<br>" +
-                    "<div style='background-color:gainsboro; font-size: 30px; margin: 10px; padding: 5px; width: 250px' >"+code+"</div><br>" +
-                    "TIL-y 서비스를 이용해주셔서 감사합니다. <br></p></div>";
-            message.setText(text, "utf-8", "html");
-
-            // 이메일로 인증코드 전송
+            message.setText(getEmailContent(code), "utf-8", "html");
             javaMailSender.send(message);
 
-            // 인증코드 생성 (유효기간: 5분)
+            // redis에 인증코드 저장 (유효기간: 5분)
             redisUtils.setDataExpire(email, code, 60*5L);
         } catch (Exception e) {
             throw new Exception500("인증코드를 전송하지 못했습니다.");
@@ -146,11 +140,18 @@ public class UserService {
 
         Random random = new Random();
 
-        return random.ints(start, end + 1)
-                .filter(i -> (i <=57 || i >=65))
-                .limit(codeLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
+        return random.ints(start, end + 1).filter(i -> (i <=57 || i >=65)).limit(codeLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+    }
+
+    public String getEmailContent(String code) {
+
+        String content = "<div style='background-color: #f8f9fa; padding: 10px 20px 50px 20px; margin: 20px'>" +
+                "<h1 style='margin: 20px'>이메일 인증</h1>" +
+                "<p>안녕하세요!<br>본인 인증을 위해 아래의 코드를 복사해 인증코드 입력 칸에 입력해주세요.<br>" +
+                "<div style='background-color:gainsboro; font-size: 30px; margin: 10px; padding: 5px; width: 250px' >"+code+"</div><br>" +
+                "TIL-y 서비스를 이용해주셔서 감사합니다. <br></p></div>";
+        return content;
     }
 
 }
