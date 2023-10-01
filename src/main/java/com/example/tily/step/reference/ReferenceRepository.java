1d5f7c101744eedc5b9e63c871bf36ab9458a2b9
hoayii <lhh@hoyaiui-MacBookAir.local>
2023-10-02 00:49:06 +0900

    feat: reference repository 구현

---
+++ src/main/java/com/example/tily/step/reference/ReferenceRepository.java
@@ -0,0 +1,7 @@
+package com.example.tily.step.reference;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReferenceRepository extends JpaRepository<Reference, Integer> {
+
+}
