a4aeb14cc9faa611f7511cc8ade852bda92aafc7
sam-mae <lsslsm2@pusan.ac.kr>
2023-10-05 01:42:29 +0900

    fix: TilControllerTest 수정

--- src/test/java/com/example/tily/til/TilControllerTest.java
+++ src/test/java/com/example/tily/til/TilControllerTest.java
@@ -129,8 +129,8 @@ public class TilControllerTest {
         result.andExpect(jsonPath("$.success").value("true"));
         result.andExpect(jsonPath("$.result.stepId").value(1L));
         result.andExpect(jsonPath("$.result.stepTitle").value("스프링 시큐리티를 사용하는 이유"));
-        result.andExpect(jsonPath("$.result.content").value("이것은 내용"));
-        result.andExpect(jsonPath("$.result.personal").value("true"));
+        result.andExpect(jsonPath("$.result.content").value("이것은 내용입니다."));
+        result.andExpect(jsonPath("$.result.personal").value("false"));
 
     }
 
