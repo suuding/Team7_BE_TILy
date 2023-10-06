d0f8ae3f5ae39783cd0908a59ee49fcecd92df0b
suuding <tngus4789@naver.com>
2023-10-07 03:20:29 +0900

    feat : 사용자가 생성한 로드맵 조회하는 쿼리 생성

--- src/main/java/com/example/tily/roadmap/RoadmapRepository.java
+++ src/main/java/com/example/tily/roadmap/RoadmapRepository.java
@@ -1,7 +1,13 @@
 package com.example.tily.roadmap;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RoadmapRepository extends JpaRepository<Roadmap, Long> {
 
+    @Query("select r from Roadmap r where r.creator.id =:userId and r.category=:category")
+    List<Roadmap> findByUserId(@Param("userId") Long userId, @Param("category") Category category);
 }
