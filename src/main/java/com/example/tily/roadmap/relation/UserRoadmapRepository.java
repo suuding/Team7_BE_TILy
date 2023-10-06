bda64c304732999793252bffd1c937afff02fa95
suuding <tngus4789@naver.com>
2023-10-07 03:18:10 +0900

    feat : user_roadmap 관계 JpaRepository 생성

---
+++ src/main/java/com/example/tily/roadmap/relation/UserRoadmapRepository.java
@@ -0,0 +1,13 @@
+package com.example.tily.roadmap.relation;
+
+import com.example.tily.roadmap.Category;
+import com.example.tily.roadmap.Roadmap;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRoadmapRepository extends JpaRepository<UserRoadmap, Long> {
+    
+}
