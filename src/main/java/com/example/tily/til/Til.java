2ea31341c0a791121c8f41c05d223917b65de182
suuding <tngus4789@naver.com>
2023-10-08 17:32:37 +0900

    feat : til 연관관계 설정

--- src/main/java/com/example/tily/til/Til.java
+++ src/main/java/com/example/tily/til/Til.java
@@ -1,6 +1,9 @@
 package com.example.tily.til;
 
+import com.example.tily.BaseTimeEntity;
+import com.example.tily.roadmap.Roadmap;
 import com.example.tily.step.Step;
+import com.example.tily.user.User;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,14 +16,23 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name="til_tb")
-public class Til {
+public class Til extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "roadmap_id")
+    private Roadmap roadmap;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="step_id")
     private Step step;
+
+    @ManyToOne(fetch =FetchType.LAZY)
+    @JoinColumn(name="writer_id")
+    private User writer;
 
     @Column(nullable = false)
     private String title;
@@ -41,11 +53,12 @@ public class Til {
     private boolean isPersonal;
 
 
-
     @Builder
-    public Til(Long id, Step step, String title, String content, String submitContent, LocalDateTime submitDate, int commentNum, boolean isPersonal) {
+    public Til(Long id, Roadmap roadmap, Step step, User writer, String title, String content, String submitContent, LocalDateTime submitDate, int commentNum, boolean isPersonal) {
         this.id = id;
+        this.roadmap = roadmap;
         this.step = step;
+        this.writer = writer;
         this.title = title;
         this.content = content;
         this.submitContent = submitContent;
