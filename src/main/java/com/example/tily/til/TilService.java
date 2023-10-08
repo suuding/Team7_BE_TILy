74d872f0360b258ab502d90a1a846be261efc03b
suuding <tngus4789@naver.com>
2023-10-08 17:34:39 +0900

    feat : 나의 틸 목록 조회하기 로직 구현

--- src/main/java/com/example/tily/til/TilService.java
+++ src/main/java/com/example/tily/til/TilService.java
@@ -3,12 +3,21 @@ package com.example.tily.til;
 import com.example.tily._core.errors.exception.Exception400;
 import com.example.tily.step.Step;
 import com.example.tily.step.StepRepository;
+import com.example.tily.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 
@@ -72,6 +81,28 @@ public class TilService {
 
         if(til.isPresent()) {
             tilRepository.deleteById(id);
+        }
+    }
+
+    @Transactional
+    public TilResponse.FindAllDTO findAllMyTil(Long roadmapId, String date, String title, int page, int size, User user) {
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdDate"));
+
+        if (date!=null) {
+            try {
+                LocalDate now = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                LocalDateTime startDate = LocalDateTime.of(now, LocalTime.of( 0,0,0));
+                LocalDateTime endDate = LocalDateTime.of(now, LocalTime.of( 23,59,59));
+
+                Slice<Til> tils = tilRepository.findAllByDateByOrderByCreatedDateDesc(user.getId(), roadmapId, startDate, endDate, title, pageable);
+                return new TilResponse.FindAllDTO(tils);
+            } catch (Exception e) {
+                throw new Exception400("입력한 날짜를 찾을 수 없습니다.");
+            }
+        } else {
+            Slice<Til> tils = tilRepository.findAllByOrderByCreatedDateDesc(user.getId(), roadmapId, title, pageable);
+            return new TilResponse.FindAllDTO(tils);
         }
     }
 }
