cfd63c1c436a6acfe0345a0e256bcd87daa7a443
suuding <tngus4789@naver.com>
2023-10-10 19:43:50 +0900

    test : 더미 데이터에 맞춰 테스트 코드 수정

--- src/test/java/com/example/tily/til/TilControllerTest.java
+++ src/test/java/com/example/tily/til/TilControllerTest.java
@@ -49,7 +49,7 @@ public class TilControllerTest {
 
         //then
         result.andExpect(jsonPath("$.success").value("true"));
-        result.andExpect(jsonPath("$.result.id").value(2));
+        result.andExpect(jsonPath("$.result.id").value(8));
 
     }
 
@@ -130,7 +130,6 @@ public class TilControllerTest {
         result.andExpect(jsonPath("$.result.stepId").value(1L));
         result.andExpect(jsonPath("$.result.stepTitle").value("스프링 시큐리티를 사용하는 이유"));
         result.andExpect(jsonPath("$.result.content").value("이것은 내용입니다."));
-        result.andExpect(jsonPath("$.result.personal").value("false"));
 
     }
 
@@ -141,7 +140,7 @@ public class TilControllerTest {
         //given
         Long roadmapId = 1L;
         Long stepId = 1L;
-        Long tilId = 5L;
+        Long tilId = 15L;
 
         //when
         ResultActions result = mvc.perform(
