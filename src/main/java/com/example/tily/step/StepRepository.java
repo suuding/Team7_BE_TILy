d30567c4ad3c0c172369fa7fb3c671b619e4f0d7
suuding <tngus4789@naver.com>
2023-10-15 18:22:44 +0900

    feat : step 조회 시 join fetch 사용한 쿼리 생성

--- src/main/java/com/example/tily/step/StepRepository.java
+++ src/main/java/com/example/tily/step/StepRepository.java
@@ -3,9 +3,14 @@ package com.example.tily.step;
 import com.example.tily.roadmap.Roadmap;
 import com.example.tily.step.reference.Reference;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StepRepository extends JpaRepository<Step, Long> {
     List<Step> findByRoadmapId(Long id);
+
+    @Query("select s from Step s join fetch s.roadmap where s.id=:id")
+    Optional<Step> findById(Long id);
 }
