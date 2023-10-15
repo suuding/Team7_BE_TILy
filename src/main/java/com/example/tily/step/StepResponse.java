5b8d419094d164090dca69e6fc350e5350575715
suuding <tngus4789@naver.com>
2023-10-15 16:09:01 +0900

    feat : step 목록 조회 응답 dto 생성

--- src/main/java/com/example/tily/step/StepResponse.java
+++ src/main/java/com/example/tily/step/StepResponse.java
@@ -1,9 +1,12 @@
 package com.example.tily.step;
 
+import com.example.tily.til.Til;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StepResponse {
     @Getter @Setter
@@ -49,6 +52,34 @@ public class StepResponse {
             this.description = step.getDescription();
             this.youtubes = youtubeLinks;
             this.references = referenceLinks;
+        }
+    }
+
+    @Getter @Setter
+    public static class FindAllStepDTO {
+        private List<StepDTO> steps;
+        private int progress;
+        private String role;
+
+        public FindAllStepDTO(List<Step> steps, Map<Step, Til> maps, int progress, String role) {
+            this.steps = steps.stream().map(step -> new StepDTO(step, maps.get(step))).collect(Collectors.toList());
+            this.progress = progress;
+            this.role = role;
+        }
+
+        @Getter @Setter
+        public class StepDTO {
+            private Long id;
+            private String title;
+            private Boolean isCompleted;
+            private Long tillId;
+
+            public StepDTO(Step step, Til til) {
+                this.id = step.getId();
+                this.title = step.getTitle();
+                this.isCompleted = til==null ? false : true;
+                this.tillId = til.getId();
+            }
         }
     }
 }
