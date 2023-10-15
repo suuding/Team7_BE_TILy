94ae3b401b8438d0059606c0f3577c9f83744db0
suuding <tngus4789@naver.com>
2023-10-16 01:56:37 +0900

    feat : alarm 목록 조회 api 생성

---
+++ src/main/java/com/example/tily/alarm/AlarmController.java
@@ -0,0 +1,22 @@
+package com.example.tily.alarm;
+
+import com.example.tily._core.security.CustomUserDetails;
+import com.example.tily._core.utils.ApiUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class AlarmController {
+
+    private final AlarmService alarmService;
+    @GetMapping("/alarms")
+    public ResponseEntity<?> findAll(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        AlarmResponse.FindAllDTO responseDTO = alarmService.findAll(userDetails.getUser());
+        return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
+    }
+}
