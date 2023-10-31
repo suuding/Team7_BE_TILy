007f68bc9e8e72333e61397d5745ffe17851f467
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-10-31 20:26:24 +0900

    feat: ImageData 생성

---
+++ src/main/java/com/example/tily/image/ImageData.java
@@ -0,0 +1,33 @@
+package com.example.tily.image;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name="image_tb")
+public class ImageData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String type;
+
+    @Lob
+    @Column(name = "imagedata", length = 1000)
+    private byte[] imageData;
+
+    @Builder
+    public ImageData(String name, String type, byte[] imageData) {
+        this.name = name;
+        this.type = type;
+        this.imageData = imageData;
+    }
+}
