511a8bb9af82e5695c09fcbea81d7a909026380e
suuding <tngus4789@naver.com>
2023-11-01 18:01:19 +0900

    refactor : StepService 예외 처리 및 리팩토링

--- src/main/java/com/example/tily/step/StepService.java
+++ src/main/java/com/example/tily/step/StepService.java
@@ -4,6 +4,7 @@ import com.example.tily._core.errors.exception.ExceptionCode;
 import com.example.tily._core.errors.exception.CustomException;
 import com.example.tily.roadmap.Roadmap;
 import com.example.tily.roadmap.RoadmapRepository;
+import com.example.tily.roadmap.relation.GroupRole;
 import com.example.tily.roadmap.relation.UserRoadmap;
 import com.example.tily.roadmap.relation.UserRoadmapRepository;
 import com.example.tily.step.reference.Reference;
@@ -17,10 +18,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Transactional(readOnly = true)
@@ -34,52 +32,51 @@ public class StepService {
     private final UserRoadmapRepository userRoadmapRepository;
     private final UserStepRepository userStepRepository;
 
+    // 개인 로드맵(카테고리)의 step 생성하기
     @Transactional
     public StepResponse.CreateIndividualStepDTO createIndividualStep(Long id, StepRequest.CreateIndividualStepDTO requestDTO, User user){
-        Roadmap roadmap = roadmapRepository.findById(id)
-                .orElseThrow(() -> new CustomException(ExceptionCode.ROADMAP_NOT_FOUND));
-        String title = requestDTO.title();
 
-        Step step = Step.builder().roadmap(roadmap).title(title).build();
+        Roadmap roadmap = getRoadmapById(id);
+
+        // 사용자가 해당 로드맵에 속했는지 확인
+        UserRoadmap userRoadmap = userRoadmapRepository.findByRoadmapIdAndUserIdAndIsAcceptTrue(id, user.getId())
+                .orElseThrow(() -> new CustomException(ExceptionCode.STEP_ROADMAP_FORBIDDEN));
+
+        Step step = Step.builder().roadmap(roadmap).title(requestDTO.title()).build(); // 개인 로드맵이므로 description, dueDate 는 null
         stepRepository.save(step);
 
-        UserStep userStep = UserStep.builder().roadmap(roadmap).step(step).user(user).isSubmit(true).build();
+        UserStep userStep = UserStep.builder().roadmap(roadmap).step(step).user(user).isSubmit(true).build(); // isSubmit true인 이유는,,
         userStepRepository.save(userStep);
 
         return new StepResponse.CreateIndividualStepDTO(step);
     }
 
-    @Transactional
+    // step의 참고자료 목록 조회
     public StepResponse.FindReferenceDTO findReference(Long stepId){
-        Step step = stepRepository.findById(stepId)
-                .orElseThrow(() -> new CustomException(ExceptionCode.STEP_NOT_FOUND));
+        Step step = getStepById(stepId);
 
-        List<Reference> referenceList = referenceRepository.findByStepId(stepId);
+        List<Reference> references = referenceRepository.findByStepId(stepId);
 
         List<StepResponse.FindReferenceDTO.YoutubeDTO> youtubeDTOs = new ArrayList<>();
-        List<StepResponse.FindReferenceDTO.WebDTO> webDTOS = new ArrayList<>();
+        List<StepResponse.FindReferenceDTO.WebDTO> webDTOs = new ArrayList<>();
 
-        for(Reference reference : referenceList){
+        for(Reference reference : references){
             String category = reference.getCategory();
             Long id = reference.getId();
             String link = reference.getLink();
 
-            if(category.equals("youtube") ) {
-                StepResponse.FindReferenceDTO.YoutubeDTO youtubeDTO = new StepResponse.FindReferenceDTO.YoutubeDTO(id, link);
-                youtubeDTOs.add(youtubeDTO);
-            }
-            else if(category.equals("web") ) {
-                StepResponse.FindReferenceDTO.WebDTO webDTO = new StepResponse.FindReferenceDTO.WebDTO(id, link);
-                webDTOS.add(webDTO);
-            }
+            if(category.equals("youtube"))
+                youtubeDTOs.add(new StepResponse.FindReferenceDTO.YoutubeDTO(id, link));
+            else if(category.equals("web"))
+                webDTOs.add(new StepResponse.FindReferenceDTO.WebDTO(id, link));
         }
 
-        return new StepResponse.FindReferenceDTO(step, youtubeDTOs, webDTOS);
+        return new StepResponse.FindReferenceDTO(step, youtubeDTOs, webDTOs);
     }
 
-    // 특정 로드맵의 step 목록 전체 조회
-    @Transactional
+    // 로드맵의 step 목록 전체 조회
     public StepResponse.FindAllStepDTO findAllStep (Long roadmapId, User user) {
+
         List<Step> steps = stepRepository.findByRoadmapId(roadmapId);
 
         Map<Step, Til> maps = new HashMap<>();
@@ -89,14 +86,20 @@ public class StepService {
         }
 
         // 해당 페이지로 들어온 사람의 역할 (master, manager, member, none)
-        UserRoadmap userRoadmap = userRoadmapRepository.findByRoadmapIdAndUserId(roadmapId, user.getId())
-                .orElseThrow(() -> new CustomException(ExceptionCode.STEP_FORBIDDEN));
+        Optional<UserRoadmap> userRoadmap = userRoadmapRepository.findByRoadmapIdAndUserIdAndIsAcceptTrue(roadmapId, user.getId());
+        String role = userRoadmap.isPresent() ? userRoadmap.get().getRole().getValue() : "none";
+        int progress = userRoadmap.isPresent() ? userRoadmap.get().getProgress() : 0;
 
-        List<StepResponse.FindAllStepDTO.StepDTO> stepDTOs = steps
-                .stream()
-                .map(step -> new StepResponse.FindAllStepDTO.StepDTO(step, maps.get(step)))
-                .collect(Collectors.toList());
+        List<StepResponse.FindAllStepDTO.StepDTO> stepDTOs = steps.stream()
+                .map(step -> new StepResponse.FindAllStepDTO.StepDTO(step, maps.get(step))).collect(Collectors.toList());
+        return new StepResponse.FindAllStepDTO(stepDTOs, progress, role);
+    }
 
-        return new StepResponse.FindAllStepDTO(stepDTOs, userRoadmap.getProgress(), userRoadmap.getRole().getValue());
+    private Roadmap getRoadmapById(Long roadmapId) {
+        return roadmapRepository.findById(roadmapId).orElseThrow(() -> new CustomException(ExceptionCode.ROADMAP_NOT_FOUND));
+    }
+
+    private Step getStepById(Long stepId) {
+        return stepRepository.findById(stepId).orElseThrow(() -> new CustomException(ExceptionCode.STEP_NOT_FOUND));
     }
 }
