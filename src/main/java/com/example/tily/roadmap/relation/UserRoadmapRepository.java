31b4bdfc0388c4a679b53da75a48dac6e2cf2b02
suuding <tngus4789@naver.com>
2023-11-02 16:20:16 +0900

    feat : UserRoadmap 조회 쿼리 작성

--- src/main/java/com/example/tily/roadmap/relation/UserRoadmapRepository.java
+++ src/main/java/com/example/tily/roadmap/relation/UserRoadmapRepository.java
@@ -11,16 +11,19 @@ import java.util.Optional;
 public interface UserRoadmapRepository extends JpaRepository<UserRoadmap, Long> {
 
     @Query("select ur.roadmap from UserRoadmap ur where ur.user.id=:userId and (ur.isAccept=:isAccept or ur.isAccept=null)")
-    List<Roadmap> findByUserId(@Param("userId") Long userId, @Param("isAccept") Boolean isAccept);
+    List<Roadmap> findByUserIdAndIsAccept(@Param("userId") Long userId, @Param("isAccept") Boolean isAccept);
 
     List<UserRoadmap> findByRoadmapIdAndIsAcceptTrue(Long roadmapId);
 
-    List<UserRoadmap> findByRoadmapIdAndIsAcceptFalse(Long roadmapId);
+    List<UserRoadmap> findByRoadmapIdAndIsAcceptFalseAndRole(Long roadmapId, String role);
 
     Optional<UserRoadmap> findByRoadmapIdAndUserIdAndIsAcceptTrue(Long roadmapId, Long userId);
 
     Optional<UserRoadmap> findByRoadmapIdAndUserIdAndIsAcceptFalse(Long roadmapId, Long userId);
 
     @Query("select ur from UserRoadmap ur where ur.roadmap.id=:roadmapId and ur.user.id=:userId")
-    Optional<UserRoadmap> findByRoadmapIdAndUserId(@Param("roadmapId") Long roadmapId,@Param("userId") Long userId);
+    Optional<UserRoadmap> findByRoadmapIdAndUserId(@Param("roadmapId") Long roadmapId, @Param("userId") Long userId);
+
+    @Query("select ur from UserRoadmap ur where ur.roadmap.id=:roadmapId and (:name is null or ur.user.name like %:name%)")
+    List<UserRoadmap> findByRoadmapIdAndIsAcceptTrueAndName(@Param("roadmapId") Long roadmapId, @Param("name") String name);
 }
