207bdf90d95a528419d0754e9dc774d096e86372
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-11-03 11:09:30 +0900

    fix: roadmap 에러 수정

--- src/main/java/com/example/tily/image/ImageResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.tily.image;
-
-public class ImageResponse {
-    public record UserImageDTO(String url){}
-
-    public record RoadmapImageDTO(String url){}
-
-    public record PostImageDTO(String url){}
-}
