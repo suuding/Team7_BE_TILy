6c1bf2dd36f4db0674760f43dc5ec18e7266b62f
suuding <tngus4789@naver.com>
2023-11-04 22:57:07 +0900

    refactor : 페이지 들어온 사람 myRole로 변수명 수정

--- src/main/java/com/example/tily/step/StepService.java
+++ src/main/java/com/example/tily/step/StepService.java
@@ -87,12 +87,12 @@ public class StepService {
 
         // 해당 페이지로 들어온 사람의 역할 (master, manager, member, none)
         Optional<UserRoadmap> userRoadmap = userRoadmapRepository.findByRoadmapIdAndUserIdAndIsAcceptTrue(roadmapId, user.getId());
-        String role = userRoadmap.isPresent() ? userRoadmap.get().getRole() : "none";
+        String myRole = userRoadmap.isPresent() ? userRoadmap.get().getRole() : "none";
         int progress = userRoadmap.isPresent() ? userRoadmap.get().getProgress() : 0;
 
         List<StepResponse.FindAllStepDTO.StepDTO> stepDTOs = steps.stream()
                 .map(step -> new StepResponse.FindAllStepDTO.StepDTO(step, maps.get(step))).collect(Collectors.toList());
-        return new StepResponse.FindAllStepDTO(stepDTOs, progress, role);
+        return new StepResponse.FindAllStepDTO(stepDTOs, progress, myRole);
     }
 
     private Roadmap getRoadmapById(Long roadmapId) {
