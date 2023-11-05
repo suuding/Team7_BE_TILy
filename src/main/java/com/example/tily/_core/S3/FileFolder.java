32163413db43f831c071d5720e2110af264fb295
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-11-05 21:22:44 +0900

    fix: SocialLoginController에서 응답 수정

--- src/main/java/com/example/tily/_core/S3/FileFolder.java
+++
@@ -1,5 +0,0 @@
-package com.example.tily._core.S3;
-
-public enum FileFolder {
-    USER_IMAGE, ROADMAP_IMAGE, POST_IMAGE
-}
--- src/main/java/com/example/tily/socialLogin/SocialLoginController.java
+++ src/main/java/com/example/tily/socialLogin/SocialLoginController.java
@@ -1,6 +1,8 @@
 package com.example.tily.socialLogin;
 
+import com.example.tily._core.utils.ApiUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,9 @@ public class SocialLoginController {
 
     // 카카오 로그인
     @GetMapping("/auth/kakao/callback")
-    public SocialLoginResponse.TokenDTO kakaoLogin(@RequestParam String code) throws JsonProcessingException {
-        return kakaoLoginService.kakaoLogin(code);
+    public ResponseEntity<?> kakaoLogin(@RequestParam String code) throws JsonProcessingException {
+        SocialLoginResponse.TokenDTO responseDTO = kakaoLoginService.kakaoLogin(code);
+
+        return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
     }
 }
