28cac785f6758bab6caf487340fe35011b5a7c76
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-11-05 21:27:25 +0900

    Revert "fix: SocialLoginController에서 응답 수정"
    This reverts commit 32163413db43f831c071d5720e2110af264fb295.

---
+++ src/main/java/com/example/tily/_core/S3/S3Service.java
@@ -0,0 +1,112 @@
+package com.example.tily._core.S3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+
+import com.amazonaws.util.IOUtils;
+import com.example.tily._core.errors.exception.CustomException;
+import com.example.tily._core.errors.exception.ExceptionCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service implements FileService {
+    private final S3Component s3Component;
+    private final AmazonS3 amazonS3;
+
+    @Override
+    public String uploadFile(MultipartFile file, FileFolder fileFolder) {
+
+        //파일 이름 생성
+        String fileName = getFileFolder(fileFolder) + createFileName(file.getOriginalFilename());
+
+        //파일 변환
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(file.getSize());
+        objectMetadata.setContentType(file.getContentType());
+
+        //파일 업로드
+        try(InputStream inputStream = file.getInputStream()) {
+            amazonS3.putObject(
+                    new PutObjectRequest(s3Component.getBucket(), fileName, inputStream, objectMetadata).withCannedAcl(CannedAccessControlList.PublicReadWrite)
+            );
+        } catch (IOException e) {
+            throw new CustomException(ExceptionCode.FILE_UPLOAD_FAIL);
+        }
+
+        return fileName;
+    }
+
+    @Override
+    public void deleteFile(String fileName) {
+        amazonS3.deleteObject(new DeleteObjectRequest(s3Component.getBucket(), fileName));
+    }
+
+    @Override
+    public String getFileUrl(String fileName) {
+        return amazonS3.getUrl(s3Component.getBucket(), fileName).toString();
+    }
+
+    @Override
+    public byte[] downloadFile(String fileName) {
+
+        //파일 유무 확인
+        validateFileExists(fileName);
+
+
+        S3Object s3Object = amazonS3.getObject(s3Component.getBucket(), fileName);
+        S3ObjectInputStream s3ObjectContent = s3Object.getObjectContent();
+
+        try {
+            return IOUtils.toByteArray(s3ObjectContent);
+        }catch (IOException e ){
+            throw new CustomException(ExceptionCode.IMAGE_DOWNLOAD_FAIL);
+        }
+    }
+
+    @Override
+    public String getFileFolder(FileFolder fileFolder) {
+
+        String folder = "";
+        if(fileFolder == FileFolder.USER_IMAGE) {
+            folder = s3Component.getUserFolder()+"/";
+        }else if(fileFolder == FileFolder.ROADMAP_IMAGE){
+            folder = s3Component.getRoadmapFolder()+"/";
+        }else if(fileFolder == FileFolder.POST_IMAGE){
+            folder = s3Component.getPostFolder()+"/";
+        }
+
+        return folder;
+    }
+
+    private void validateFileExists(String fileName) {
+        if(!amazonS3.doesObjectExist(s3Component.getBucket(), fileName))
+            throw new CustomException(ExceptionCode.IMAGE_NOT_FOUND);
+    }
+
+    //파일 이름 생성 로직
+    private String createFileName(String originalFileName) {
+        return UUID.randomUUID().toString().concat(getFileExtension(originalFileName));
+    }
+
+    //파일의 확장자명을 가져오는 로직
+    private String getFileExtension(String fileName){
+        try{
+            return fileName.substring(fileName.lastIndexOf("."));
+        }catch(StringIndexOutOfBoundsException e) {
+            throw new CustomException(ExceptionCode.INVALID_FILE_FORMAT);
+        }
+    }
+
+    //이미지 URL->파일 이름 변환
+    public static String convertToFileName(String imageUrl){
+        String[] path = imageUrl.split("/");
+        return path[path.length-2] + "/" + path[path.length-1];  //폴더 이름 + 파일 이름
+    }
+}
--- src/main/java/com/example/tily/socialLogin/SocialLoginController.java
+++ src/main/java/com/example/tily/socialLogin/SocialLoginController.java
@@ -1,8 +1,6 @@
 package com.example.tily.socialLogin;
 
-import com.example.tily._core.utils.ApiUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +17,7 @@ public class SocialLoginController {
 
     // 카카오 로그인
     @GetMapping("/auth/kakao/callback")
-    public ResponseEntity<?> kakaoLogin(@RequestParam String code) throws JsonProcessingException {
-        SocialLoginResponse.TokenDTO responseDTO = kakaoLoginService.kakaoLogin(code);
-
-        return ResponseEntity.ok().body(ApiUtils.success(responseDTO));
+    public SocialLoginResponse.TokenDTO kakaoLogin(@RequestParam String code) throws JsonProcessingException {
+        return kakaoLoginService.kakaoLogin(code);
     }
 }
