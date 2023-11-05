b2857d0833adea17407ae0b5176132eb81e022a3
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-11-05 14:20:44 +0900

    refactor: sendRequest를 통해 KakaoLoginService 리팩토링

--- src/main/java/com/example/tily/socialLogin/KakaoLoginService.java
+++ src/main/java/com/example/tily/socialLogin/KakaoLoginService.java
@@ -64,19 +64,11 @@ public class KakaoLoginService {
         body.add("code", code);
 
         // HTTP 요청 보내기
-        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(body, headers);
-        RestTemplate rt = new RestTemplate();
-        ResponseEntity<String> response = rt.exchange(
-                "https://kauth.kakao.com/oauth/token",
-                HttpMethod.POST,
-                kakaoTokenRequest,
-                String.class
-        );
+        String response = sendRequest("https://kauth.kakao.com/oauth/token", HttpMethod.POST, body, headers);
 
         // 액세스 토큰 파싱
-        String responseBody = response.getBody();
         ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(responseBody);
+        JsonNode jsonNode = objectMapper.readTree(response);
 
         return jsonNode.get("access_token").asText();
     }
@@ -88,19 +80,11 @@ public class KakaoLoginService {
         headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
         // HTTP 요청 보내기
-        HttpEntity<MultiValueMap<String, String>> kakaoUserInfoRequest = new HttpEntity<>(headers);
-        RestTemplate rt = new RestTemplate();
-        ResponseEntity<String> response = rt.exchange(
-                "https://kapi.kakao.com/v2/user/me",
-                HttpMethod.POST,
-                kakaoUserInfoRequest,
-                String.class
-        );
+        String response = sendRequest("https://kapi.kakao.com/v2/user/me", HttpMethod.POST, null, headers);
 
         // responseBody에 있는 정보를 꺼냄
-        String responseBody = response.getBody();
         ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(responseBody);
+        JsonNode jsonNode = objectMapper.readTree(response);
 
         Long id = jsonNode.get("id").asLong();
         String email = jsonNode.get("kakao_account").get("email").asText();
@@ -148,5 +132,12 @@ public class KakaoLoginService {
         CustomUserDetails customUserDetails = ((CustomUserDetails) authentication.getPrincipal());
         String token = JWTProvider.createAccessToken(customUserDetails.getUser());
         response.addHeader("Authorization", "BEARER" + " " + token);
+    }
+
+    private String sendRequest(String url, HttpMethod method, MultiValueMap<String, String> body, HttpHeaders headers) {
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response = rt.exchange(url, method, request, String.class);
+        return response.getBody();
     }
 }
