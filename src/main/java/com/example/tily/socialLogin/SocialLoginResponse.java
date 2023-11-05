def97a1723b69ef2859f039bc65995c68f937ede
suuding <tngus4789@naver.com>
2023-11-05 23:49:29 +0900

    fix : 응답 DTO 수정

--- src/main/java/com/example/tily/socialLogin/SocialLoginResponse.java
+++ src/main/java/com/example/tily/socialLogin/SocialLoginResponse.java
@@ -3,7 +3,7 @@ package com.example.tily.socialLogin;
 public class SocialLoginResponse{
     public record UserInfoDto(String nickname, String email){};
 
-    public record TokenDTO(String token){};
+    public record TokenDTO(String accessToken){};
 }
 
 
