eec9952413ee121d6a4e90676e6eba5c6521c46d
suuding <tngus4789@naver.com>
2023-11-08 12:15:27 +0900

    refactor : step 응답 추가 및 리팩토링

--- src/main/java/com/example/tily/step/StepRequest.java
+++ src/main/java/com/example/tily/step/StepRequest.java
@@ -1,9 +1,25 @@
 package com.example.tily.step;
 
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 
 public class StepRequest {
 
-    public record CreateIndividualStepDTO(@NotBlank(message = "스텝 제목을 입력해주세요.") String title) {
-    }
+    public record CreateIndividualStepDTO(
+            @NotBlank(message = "스텝 제목을 입력해주세요.") String title
+    ) { }
+
+    public record CreateStepDTO(
+            @NotBlank(message = "step의 제목을 입력해주세요.")
+            String title,
+            Long roadmapId,
+            String description,
+            LocalDateTime dueDate
+    ) { }
+
+    public record UpdateStepDTO(
+            String title,
+            String description,
+            LocalDateTime dueDate
+    ) { }
 }
