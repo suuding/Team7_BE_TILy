977a314b83561bcd1a41bf4725c11c2c40a368b2
suuding <tngus4789@naver.com>
2023-11-08 12:17:06 +0900

    feat : til의 title 수정 추가

--- src/main/java/com/example/tily/til/Til.java
+++ src/main/java/com/example/tily/til/Til.java
@@ -68,6 +68,10 @@ public class Til extends BaseTimeEntity {
         this.content  = content;
     }
 
+    public void updateTitle (String title) {
+        this.title = title;
+    }
+
     public void submitTil(String submitContent) {
         this.content  = submitContent;
         this.submitContent = submitContent;
