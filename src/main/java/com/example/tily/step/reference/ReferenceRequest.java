efb159fcd52b609e63ae4a764445ab199864fec3
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-11-08 09:37:09 +0900

    fix: ReferenceController 수정

--- src/main/java/com/example/tily/step/reference/ReferenceRequest.java
+++ src/main/java/com/example/tily/step/reference/ReferenceRequest.java
@@ -3,6 +3,8 @@ package com.example.tily.step.reference;
 import java.time.LocalDateTime;
 
 public class ReferenceRequest {
+    public record FindReferenceDTO(Long stepId){
+    }
 
     public record CreateReferenceDTO(String category, String link) {
     }
