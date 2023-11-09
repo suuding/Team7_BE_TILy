ad8948438858a58b5776931d66af70f3314a1009
suuding <tngus4789@naver.com>
2023-11-09 17:44:40 +0900

    feat : reference api 수정

--- src/main/java/com/example/tily/step/reference/ReferenceRequest.java
+++ src/main/java/com/example/tily/step/reference/ReferenceRequest.java
@@ -3,9 +3,9 @@ package com.example.tily.step.reference;
 import java.time.LocalDateTime;
 
 public class ReferenceRequest {
-    public record FindReferenceDTO(Long stepId){
-    }
-
-    public record CreateReferenceDTO(Long stepId, String category, String link) {
-    }
+    public record CreateReferenceDTO(
+            Long stepId,
+            String category,
+            String link
+    ) { }
 }
