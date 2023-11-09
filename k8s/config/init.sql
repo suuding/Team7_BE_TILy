728fbf531f62a7ff1cce35b3806d77f9ee247fcc
tngus4789 <tngus4789@naver.com>
2023-11-09 15:47:33 +0900

    modify

--- k8s/config/init.sql
+++ k8s/config/init.sql
@@ -10,6 +10,7 @@ CREATE TABLE user_tb (
     password VARCHAR(255) NOT NULL,
     image VARCHAR(255),
     role VARCHAR(255),
+    is_deleted BOOLEAN,
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -19,13 +20,14 @@ CREATE TABLE roadmap_tb (
     category VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     creator_id BIGINT,
-    description VARCHAR(255),
+    description TEXT,
     image VARCHAR(255),
     step_num INT,
     current_num BIGINT,
     code VARCHAR(255),
     is_public BOOLEAN,
     is_recruit BOOLEAN,
+    is_deleted BOOLEAN,
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -34,20 +36,22 @@ CREATE TABLE step_tb (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     roadmap_id BIGINT,
-    description VARCHAR(255),
+    description TEXT,
+    is_deleted BOOLEAN,
     due_date TIMESTAMP
 );
 
 CREATE TABLE til_tb (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    content VARCHAR(255),
-    submit_content VARCHAR(255),
+    content TEXT,
+    submit_content TEXT,
     roadmap_id BIGINT,
     step_id BIGINT,
     writer_id BIGINT,
     comment_num INT,
     is_personal BOOLEAN,
     submit_date TIMESTAMP,
+    is_deleted BOOLEAN,
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -56,7 +60,8 @@ CREATE TABLE comment_tb (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     til_id BIGINT,
     writer_id BIGINT,
-    content VARCHAR(255),
+    content TEXT,
+    is_deleted BOOLEAN,
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -65,17 +70,19 @@ CREATE TABLE reference_tb (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     step_id BIGINT,
     category VARCHAR(255) NOT NULL,
-    link VARCHAR(255) NOT NULL
+    link TEXT,
+    is_deleted BOOLEAN
 );
 
 CREATE TABLE user_roadmap_tb (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    content VARCHAR(255),
+    content TEXT,
     is_accept BOOLEAN,
     progress INT NOT NULL,
     role VARCHAR(255) NOT NULL,
     roadmap_id BIGINT,
     user_id BIGINT,
+    is_deleted BOOLEAN,
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -85,7 +92,8 @@ CREATE TABLE user_step_tb (
     is_submit BOOLEAN NOT NULL,
     roadmap_id BIGINT,
     step_id BIGINT,
-    user_id BIGINT
+    user_id BIGINT,
+    is_deleted BOOLEAN
 );
 
 CREATE TABLE alarm_tb (
@@ -97,6 +105,15 @@ CREATE TABLE alarm_tb (
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE image (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    image_path varchar(255),
+    original_image_name varchar(255),
+    storage_image_name varchar(255)
+);
+
+
 ALTER TABLE alarm_tb
 ADD FOREIGN KEY (til_id)
 REFERENCES til_tb(id);
