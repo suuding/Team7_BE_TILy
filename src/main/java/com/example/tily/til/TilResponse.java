dd13b4d58d06773d68750edc8a7fb17beaec228a
suuding <tngus4789@naver.com>
2023-11-10 01:43:22 +0900

    fix : til 조회 응답 DTO 추가

--- src/main/java/com/example/tily/til/TilResponse.java
+++ src/main/java/com/example/tily/til/TilResponse.java
@@ -54,9 +54,9 @@ public class TilResponse {
             }
         }
 
-        public record RoadmapDTO(Long id, String name) {
+        public record RoadmapDTO(Long id, String name, String category) {
             public RoadmapDTO(Roadmap roadmap) {
-                this(roadmap.getId(), roadmap.getName());
+                this(roadmap.getId(), roadmap.getName(), roadmap.getCategory().getValue());
             }
         }
     }
