3d67447593cfa8c01911ce234ca9beb656b40a40
tngus4789 <tngus4789@naver.com>
2023-11-11 05:59:01 +0900

    fix : redirect_uri

--- src/main/java/com/example/tily/user/socialLogin/KakaoLoginService.java
+++ src/main/java/com/example/tily/user/socialLogin/KakaoLoginService.java
@@ -60,7 +60,7 @@ public class KakaoLoginService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", "872b661d1b5d025d01a76fdb6936f3fb");
-        body.add("redirect_uri", "http://localhost:3000/auth/kakao/callback");
+        body.add("redirect_uri", "https://kc29be941feb6a.user-app.krampoline.com/auth/kakao/callback");
         body.add("code", code);
 
         // HTTP 요청 보내기
