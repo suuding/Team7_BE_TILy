bd2954ef019c91a906d02b67adeb26dcb5ac2c89
hoyaii <131665728+hoyaii@users.noreply.github.com>
2023-11-11 03:14:07 +0900

    refactor: S3Service 리팩토링

--- src/main/java/com/example/tily/_core/S3/FileFolder.java
+++ src/main/java/com/example/tily/_core/S3/FileFolder.java
@@ -1,5 +1,24 @@
 package com.example.tily._core.S3;
 
 public enum FileFolder {
-    USER_IMAGE, ROADMAP_IMAGE, POST_IMAGE
+    USER_IMAGE {
+        @Override
+        public String getFolder(S3Component component) {
+            return component.getUserFolder() + "/";
+        }
+    },
+    ROADMAP_IMAGE {
+        @Override
+        public String getFolder(S3Component component) {
+            return component.getRoadmapFolder() + "/";
+        }
+    },
+    POST_IMAGE {
+        @Override
+        public String getFolder(S3Component component) {
+            return component.getPostFolder() + "/";
+        }
+    };
+
+    public abstract String getFolder(S3Component component);
 }
